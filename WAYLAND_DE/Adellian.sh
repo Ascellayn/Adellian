47fa143d198ff8883bf1a63a97308512952c9ea4
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2024-09-20 09:44:59 +0200

    more bullshit

--- WAYLAND_DE/Adellian.sh
+++ WAYLAND_DE/Adellian.sh
@@ -24,3 +24,20 @@ Install_BaseSystem() {
     apt-get install --no-install-recommends -y \
     htop ufw wget # i can't be fucked finishing this rn whatev, i dont remember all the packages i need damn it
 }
+
+Minimize_System() {
+    # These packages are getting removed regardless of what's selected
+    echo "Minimizing the system..."
+    apt-get purge -y \
+    eject laptop-detect \
+    vim-common vim-tiny \
+    grub-common fdisk
+    # These packages will require confirmation by the user to delete them or not. (Right now they don't because I'm lazy)
+    # If it looks ridiculous that's because it is.
+    apt-get purge -y os-prober
+    apt-get purge -y iproute2
+    apt-get purge -y iputils-ping
+
+    # Cleanup
+    apt-get autoremove --purge -y
+}

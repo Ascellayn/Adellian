579304eddcfaef13de364294f332c0656022c122
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2024-09-30 18:25:02 +0000

    Delete WAYLAND_DE/rootfs/System/SystemKeys directory

--- WAYLAND_DE/rootfs/System/SystemKeys/System_Shutdown.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo -e "\e[31m=== AUTHENTIFICATION REQUIRED ===\e[0m"
-echo "Please enter the root user's password to shutdown."
-su - -c "shutdown now"
--- WAYLAND_DE/rootfs/System/SystemKeys/Terminal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/bin/bash /System/System_Banner.sh
-/bin/bash

63d3ae435f0473e2a9ecf6696815bcd48b31858b
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-02-19 23:22:36 +0100

    Update NoVideo.sh

--- WAYLAND_DE/NoVideo.sh
+++ WAYLAND_DE/NoVideo.sh
@@ -1,5 +1,6 @@
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
+apt update
 apt install linux-headers-amd64 --no-install-recommends -y
-#apt install cuda-drivers -y # untested rn with my config
-apt install nvidia-open -y
+apt install cuda-drivers -y
+rm -rf cuda-keyring_1.1-1_all.deb

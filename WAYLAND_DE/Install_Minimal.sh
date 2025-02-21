5e71efa98b30b93e7a6e3a8c0f21e39dadfbbd46
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-02-21 18:32:22 +0100

    whatever the hell this is

--- WAYLAND_DE/Install_Minimal.sh
+++ WAYLAND_DE/Install_Minimal.sh
@@ -1,44 +1,14 @@
-echo "Removing useless packages..."
-apt purge laptop-detect os-prober eject vim* dictionaries-common wamerican ibritish iamerican fdisk --allow-remove-essential
-apt autoremove --purge
+echo "Removing unused packages..."
+apt purge laptop-detect os-prober eject vim* fdisk --allow-remove-essential -y
+apt autoremove --purge -y
 
 echo "Installing Terminal Tools..."
 # Terminal Tools
-apt install --no-install-recommends -y      \
+apt install --no-install-recommends -y     \
 htop wget git gpg tmux                  \
 
-echo "Adding Firefox Nightly Repository..."
-# Firefox Nightly
-install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list
-
-echo "Adding VSCode Insiders Repository..."
-# VSCode
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
-rm -f packages.microsoft.gpg
-
-echo "Updating repositories..."
-apt update
-apt upgrade
-
-echo "Installing the Desktop Environnement and its essential tools..."
-# Desktop Environement & Essential Tools
-apt install --no-install-recommends         \
-wireplumber pipewire                    \
-hyprland foot tofi waybar swaybg pavucontrol        \
-thunar firefox-nightly                  \
-librsvg2-common                     \
-
-echo "Installing non-essential stuff..."
-# Non-essential Stuff
-apt install --no-install-recommends         \
-code-insiders
-
 echo "Installing temporary dependencies..."
-apt install --no-install-recommends sassc dconf-cli
+apt install --no-install-recommends sassc dconf-cli -y
 cd /tmp
 echo "Downloading Fluent GTK & Icon Theme..."
 git clone https://github.com/vinceliuice/Fluent-gtk-theme
@@ -56,8 +26,8 @@ echo "Merging Adellian rootfs..."
 cd TBD_Name
 cd WAYLAND_DE
 dconf load / < adellian_dconf.txt
-apt purge dconf-cli sassc
-apt autoremove --purge
+apt purge dconf-cli sassc -y
+apt autoremove --purge -y
 cd rootfs
 cp -R -v * /
 cd /tmp
@@ -65,4 +35,40 @@ echo "Cleaning up..."
 rm -rf Fluent-gtk-theme
 rm -rf Fluent-icon-theme
 rm -rf TBD_Name
-echo "Done."
+
+echo "Updating sources and migrating to Experimental..."
+apt update
+apt upgrade -y --no-install-recommends
+apt update -t experimental
+apt upgrade -y --no-install-recommends -t experimental
+
+echo "Adding Firefox Nightly Repository..."
+# Firefox Nightly
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | tee /etc/apt/keyrings/packages.mozilla.org.asc
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list
+
+echo "Adding VSCode Insiders Repository..."
+# VSCode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+
+echo "Updating repositories..."
+apt update
+
+echo "Installing the Desktop Environnement and its essential tools..."
+# Desktop Environement & Essential Tools
+apt install --no-install-recommends -y -t experimental         \
+wireplumber pipewire                    \
+hyprland foot tofi waybar swaybg pavucontrol        \
+thunar firefox-nightly                  \
+librsvg2-common                     \
+
+echo "Installing non-essential stuff..."
+# Non-essential Stuff
+apt install --no-install-recommends         \
+code-insiders
+
+echo "Done"

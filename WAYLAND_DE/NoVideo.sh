8e6559486c93c5b6f72a174af4f99e037a26945d
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-03-22 22:29:01 +0000

    Nvidia VAAPI thigny

--- WAYLAND_DE/NoVideo.sh
+++ WAYLAND_DE/NoVideo.sh
@@ -6,3 +6,4 @@ apt install linux-headers-amd64 --no-install-recommends -y -t experimental
 apt install cuda-drivers -y # Not using -t experimental here because it usually fails for some reason
 rm -rf cuda-keyring_1.1-1_all.deb
 apt install nvidia-driver-libs:i386 -y # This is for steam
+apt install nvidia-vaapi-driver -t experimental -y # Don't remember what it's for exactly, but Hyprland benefits from it or some shid, something about hardware acceleration and setting a specific env var for this

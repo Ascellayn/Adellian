c509e2c9418f99532033acc86b3713f1d5568ae7
Ascellayn <root.siriusb@gmail.com>
2025-08-12 00:58:00 +0200

    =Siriollian= (WIP) Base Installation Script

---
+++ Install-Scripts/Siriollian/Base_Installation.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Base Installation Script for Adellian's Server Branch
+SEPARATOR=$(printf "%-$(tput cols)s\n" "" | tr " " "=")
+LOG_FILE="/System/Logs/Adellian_Installer/Siriollian_Branch.log"
+
+SEPARATE() {
+	printf "${SEPARATOR}" >> ${LOG_FILE}
+}
+
+LOG() {
+	printf "$1"
+	printf "$1" >> ${LOG_FILE}
+}
+
+ENDL() {
+	printf "\n" >> ${LOG_FILE}
+}
+
+cd /System/Library
+mkdir SirioTTY
+cd SirioTTY
+
+ENDL
+LOG "[SirioTTY] - Installing Base System...\n"
+SEPARATE
+{
+	apt install --no-install-recommends -y	\
+	tmux htop openssh-server				\
+	python3-venv ufw wireguard				\
+} &>> ${LOG_FILE}
+
+
+# TBD:
+# Install Adellian Server Tools such as the Wireguard Configuration Tool
+# Configure UFW
+# Install Siriollian RootFS

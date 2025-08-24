640d49fb9702f377b839a64833b7b6ffba8003e4
Ascellayn <root.siriusb@gmail.com>
2025-08-24 15:24:52 +0200

    Fix ignition due to dumb copy paste

--- Ignition.sh
+++ Ignition.sh
@@ -14,13 +14,12 @@ ENDL
 LOG "[Ignition] - Installing WGET...\n"
 {
 	apt install wget -y --no-install-recommends
-} &>> ${LOG_FILE}
+} &> /dev/null
 
-ENDL
 LOG "[Ignition] - Downloading Adellian's Bootstrap Script...\n"
 {
 	wget "https://ascellayn.github.io/Adellian/Bootstrap.sh" -O Bootstrap.sh
-} &>> ${LOG_FILE}
+} &> /dev/null
 
 chmod +x Bootstrap.sh # Make sure the fucker is executable
 LOG "\n\n\nScript finished. Run './Bootstrap.sh' to proceed with the installation.\n"

a00fa579b4e366ac353eb6d4d5da2c82d166d2f3
Ascellayn <root.siriusb@gmail.com>
2025-09-01 12:36:58 +0200

    Additional Software: QEMU

---
+++ Install-Scripts/Universal/QEMU.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Base Installation Script for Adellian's Server Branch
+SEPARATOR=$(printf "%-$(tput cols)s\n" "" | tr " " "=")
+LOG_FILE="/System/Logs/Adellian_Installer/Universal_QEMU.log"
+
+SEPARATE() {
+	printf "${SEPARATOR}" >> ${LOG_FILE}
+}
+
+LOG() {
+	printf "$1"
+	printf "$1" >> ${LOG_FILE}
+}
+
+ENDL() {
+	printf "\n" >> ${LOG_FILE}
+}
+
+mkdir -p /System/LSW-VM
+cd /System/LSW-VM
+
+ENDL
+LOG "[QEMU] - Installing Dependencies...\n"
+SEPARATE
+{
+	apt install --no-install-recommends -y	\
+	qemu-system-amd64 qemu-utils qemu-system-gui qemu-utils ovmf
+} &>> ${LOG_FILE}
+
+
+# RootFS is handled by the Adellian Installer automatically.

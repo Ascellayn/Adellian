5f81338a47e0102bd68bbab2495c8a9727eb7fc0
Ascellayn <root.siriusb@gmail.com>
2025-09-03 17:28:56 +0200

    Thunar | Add script

---
+++ Install-Scripts/Universal/Thunar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SEPARATOR=$(printf "%-$(tput cols)s\n" "" | tr " " "=")
+LOG_FILE="/System/Logs/Adellian_Installer/Universal_Thunar.log"
+
+SEPARATE() {
+	printf "${SEPARATOR}" >> ${LOG_FILE}
+}
+
+LOG() {
+	printf "$1"
+	printf "$1" >> ${LOG_FILE}
+}
+
+ENDL() {
+	printf "\n" >> ${LOG_FILE}
+}
+
+mkdir -p /System/LSW-VM
+cd /System/LSW-VM
+
+ENDL
+LOG "[Thunar] - Installing Thunar...\n"
+SEPARATE
+{
+	apt install --no-install-recommends -y	\
+	thunar tumbler-common librsvg2-common
+} &>> ${LOG_FILE}
+
+
+# Configuration is handled by the corresponding Adellian Branch.

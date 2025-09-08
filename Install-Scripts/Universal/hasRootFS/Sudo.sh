304def26153fda87cea5680e54b7e52d8701a8fc
Ascellayn <root.siriusb@gmail.com>
2025-09-08 16:13:38 +0200

    Sudo | Automatically add Adellian User as a Sudoer

--- Install-Scripts/Universal/hasRootFS/Sudo.sh
+++ Install-Scripts/Universal/hasRootFS/Sudo.sh
@@ -4,6 +4,9 @@
 SEPARATOR=$(printf "%-$(tput cols)s\n" "" | tr " " "=")
 LOG_FILE="/System/Logs/Adellian_Installer/Universal_Sudo.log"
 
+ADELLIAN_BRANCH=$1
+ADELLIAN_USER=$2
+
 SEPARATE() {
 	printf "${SEPARATOR}" >> ${LOG_FILE}
 }
@@ -25,5 +28,10 @@ SEPARATE
 	sudo
 } &>> ${LOG_FILE}
 
+ENDL
+LOG "[Sudo] - Allowing $ADELLIAN_USER to use sudo...\n"
+printf "# Adellian - Sudoer Autoconfiguration
+$ADELLIAN_USER ALL=(ALL:ALL) ALL
+" > /etc/sudoers.d/$ADELLIAN_USER
 
 # RootFS is handled by the Adellian Installer automatically.
